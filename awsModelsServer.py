496ca7fadf32480d115c6489da5982ba3dc2c9be
timothy-voiuhy <ezratimothy600@gmail.com>
2024-10-26 11:16:16 +0300

    daily updates

--- awsModelsServer.py
+++ awsModelsServer.py
@@ -3,10 +3,14 @@ import random
 import os
 import sys
 import hashlib
+import torch
 import logging
 from ultralytics import YOLO
+import argparse
+import cv2
 
-MODEL_PATH = "/home/"
+LOG_DIR = "/home/ec2-user/AI_ML/LOGS/"
+MODEL_PATH = "/home/ec2-user/AI_ML/Yolo-LiveFeed/Models/CrackSegBest.pt"
 
 class awsModelServer:
     def __init__(self) -> None:
@@ -38,8 +42,9 @@ def createLogger(log_level = None, name = None, mode = 'a', log_format = None, i
     return logger
 
 console_logger = createLogger(is_consoleLogger=True)
+file_logger = createLogger(log_level=logging.INFO, filename=LOG_DIR+"model.log")
 
-def predict_single_image(image_path, model=None, device=None, model_path=None):
+def predict_single_image(image_path, model=None, device=None):
     """Predicts the class of a single crack image.
 
     Args:
@@ -50,11 +55,36 @@ def predict_single_image(image_path, model=None, device=None, model_path=None):
     Returns:
         The predicted class (0 for no crack, 1 for crack).
     """
+    image = cv2.imread(image_path)
+    try:
+        results = model.predict(image)
+        for result in results:
+            boxes = result.boxes.xyxy # get the boxes coordinates 
+            confidences = result.boxes.conf  # get the confidence levels 
+            class_ids = result.boxes.cls # get the class ids 
+            class_names = result.names 
+            
+            for box, confidence, class_id  in zip(boxes, confidences, class_ids):
+                # capture the corners 
+                x1, y1, x2, y2 = map(int, box)
+                # rectangle for the bounding box
+                cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                # direction 
+                # box_tensor = torch.from_numpy(box)
+                # direction = calculateDirection(image=image_tensor, boundingbox=(x1, y1, x2, y2))
+                # label = f"Direction: {direction}"
 
+                label = f"Conf: {confidence:.2f} Class: {class_names[class_id.item()]}"
+                #place the label onto the image at a spcific point 
+                cv2.putText(image, label, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                return image
+    except Exception as e:
+        file_logger.error(f"Encountered error: {e}")
+        return f"error: {e}".encode()
 
 async def handleClient(reader:asyncio.StreamReader, writer:asyncio.StreamWriter):
     addr = writer.get_extra_info('peername')
-    logging.info(f"Connected to : {addr}")
+    file_logger.info(f"Connected to : {addr}")
     # read the data sent by the client
     image_name = "received_image"+str(random.randint(0, 10000))
     hashed_image_name = hashlib.md5(image_name.encode()).hexdigest()+".jpg"; # to make sure the names are different
@@ -65,14 +95,17 @@ async def handleClient(reader:asyncio.StreamReader, writer:asyncio.StreamWriter)
                 break # end data stream 
             image_file.write(data[:-len(b"END_OF_IMAGE")])
         logging.info("Image recieved from client")
-    predicted_class = int(predict_single_image(hashed_image_name))
-    writer.write(predicted_class.to_bytes(4, byteorder ="little"))
+    image = predict_single_image(hashed_image_name)
+    if isinstance(image, bytes):
+        writer.write(image)
+    else:
+        writer.write(image.tobytes( order="little"))
     await writer.drain()
     os.remove(hashed_image_name)
     writer.close()
     await writer.wait_closed()
 
-async def runServer(address = "127.0.0.1", server_port = 8082):
+async def runServer(address = "127.0.0.1", server_port = None):
     try:
         server = await asyncio.start_server(handleClient, address, server_port)
         addr = server.sockets[0].getsockname()
@@ -83,3 +116,19 @@ async def runServer(address = "127.0.0.1", server_port = 8082):
         logging.info("Closing server")
         server.close()
         await server.wait_closed()
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p" "--port", type=int, help="the port onto which to run the server. This must be exposed on the ec2 instace")
+    return parser
+if __name__ == "__main__":
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+    model = YOLO(MODEL_PATH)
+
+    parser = parseArgs()
+    args = parser.parse_args()
+    if args.port:
+        runServer(server_port=args.port)
